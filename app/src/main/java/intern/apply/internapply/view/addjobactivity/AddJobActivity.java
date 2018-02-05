4d61f906df85a91f3f2d53c5e5f27e7c4baf5be5
EnoshAnwar <enosh.anwar12@gmail.com>
2018-02-05 03:50:46 -0600

    Created Add Job Activity

---
+++ app/src/main/java/intern/apply/internapply/view/addjobactivity/AddJobActivity.java
@@ -0,0 +1,15 @@
+package intern.apply.internapply.view.addjobactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import intern.apply.internapply.R;
+
+public class AddJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_job);
+    }
+}
